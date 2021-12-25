af3d49df1fdabc847e25e8a8e09bf909de99d492
Ibrahim <42994133+ContraHacker@users.noreply.github.com>
2021-12-25 09:38:56 -0800

    Add First Page

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,5 @@
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
+export default function ContraHacker({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
-
-export default MyApp
