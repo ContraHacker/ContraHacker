2fdfff3a39a187c176e811e8d1da1eb6c044a9da
ContraHacker <ifchinar@gmail.com>
2022-01-10 22:14:24 +0530

    Add Jumbo Section & Google Analytics

--- pages/index.js
+++ pages/index.js
@@ -3,14 +3,19 @@ import styles from '../styles/Index.module.scss';
 
 export default function Index() {
 
-  return (
-    <>
-    <Head>
-      <title>ContraHacker | Ibrahim Farooqui</title>
-    </Head>
-    <main className = { styles.main }>
-      <h1 className = { styles.jumbo } >Website Coming Soon</h1>
-    </main>
-    </>
-  )
+    return (
+        <>
+            <Head>
+                <title>ContraHacker | Ibrahim Farooqui</title>
+            </Head>
+            <main className = { styles.main }>
+
+                <div className = { styles.jumbo }>
+                    <p>Ibrahim Farooqui</p>
+                    <h1>Contra<br />Hacker</h1>
+                </div>
+
+            </main>
+        </>
+    );
 }
